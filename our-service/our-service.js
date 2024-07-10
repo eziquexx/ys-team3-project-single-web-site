d599806e3446088bb6af888ed301c94326af7c57
jelee <waftyann@gmail.com>
2024-07-10 14:58:02 +0900

    소스코드 정리

--- our-service/our-service.js
+++ our-service/our-service.js
@@ -1,11 +1,4 @@
 
-
-// our service 위치값 찾아서
-// 해당 위치에 도달하면
-// item들이 opacity가 0 -> 1로
-// transition y값이 밑에서 위로
-// dration으로 시간차로 나타나게 하기.
-
 $(function(){
 
   $(window).on('scroll', function(){
@@ -14,11 +7,6 @@ $(function(){
     let svWrapH = $('#ourService').innerHeight();
     let workScrollT = $('#work').offset().top;
     let workWrapH = $('#work').innerHeight();
-
-    // console.log('window scroll 위치', svTop);
-    // console.log('our service 위치', svScrollT);
-    // console.log('.wrap 위치', wrapH);
-   
 
     // section#ourService 애니메이션 효과
     if(windowTop > (svScrollT - (svWrapH/2))){
@@ -83,59 +71,8 @@ $(function(){
         })
       });
 
-
-      // $({ Counter: 0 }).animate({
-      //   Counter: $('.Single').text()
-      // }, {
-      //   duration: 1000,
-      //   easing: 'swing',
-      //   step: function () {
-      //     $('.Single').text(Math.ceil(this.Counter));
-      //   }
-      // });
-
-
-      ///////////////////////////////////////////////////////////
-      // let index = 0;
-
-      // let test = setInterval(num1, 30);
-      // function num1(){
-      //   if(index <= wTextNum1){
-      //     $('#work .workItem1 span.wItemNumText').text(index);
-      //     index += 2;
-      //   }
-      // }
-
-      // let test1 = setInterval(num2, 50);
-      // function num2(){
-      //   if(index <= wTextNum2){
-      //     $('#work .workItem2 span.wItemNumText').text(index);
-      //     index += 20;
-      //   }
-      // }
-
-      // setInterval(num3, 50);
-      // function num3(){
-      //   if(index <= wTextNum3){
-      //     $('#work .workItem3 span.wItemNumText').text(index);
-      //     index += 20;
-      //   }
-      // }
-
-      // setInterval(num4, 50);
-      // function num4(){
-      //   if(index <= wTextNum4){
-      //     $('#work .workItem4 span.wItemNumText').text(index);
-      //     index += 20;
-      //   }
-      // }
-
-
     }; // #work scroll event end
 
-    
-  
-  
   }); // window scroll event end
 
 });
