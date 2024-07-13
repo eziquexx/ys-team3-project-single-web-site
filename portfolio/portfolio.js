9514c25b1a64278197d1684fe577a5eff4880624
waftyann <waftyann@gmail.com>
2024-07-14 08:45:38 +0900

    6차 수정 portfolio 애니메이션 시간 수정

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -5,35 +5,35 @@ $(function(){
     let windowTop = $(this).scrollTop();
     let pfScrollT = parseInt($('#portfolio').offset().top);
     // contact scroll event
-    if(windowTop > (pfScrollT-200)){
+    if(windowTop > (pfScrollT-400)){
       $('#portfolio li:nth-child(1) a').animate({
         top : '0'
-      }, 1000, "easeOutQuad");
+      }, 800, "easeOutQuad");
       $('#portfolio li:nth-child(2) a').animate({
         top : '0'
-      }, 1400, "easeOutQuad");
+      }, 1000, "easeOutQuad");
       $('#portfolio li:nth-child(3) a').animate({
         top : '0'
-      }, 1800, "easeOutQuad");
+      }, 1200, "easeOutQuad");
       $('#portfolio li:nth-child(4) a').animate({
         top : '0'
-      }, 2200, "easeOutQuad");
+      }, 1400, "easeOutQuad");
     }// contact scroll event end
 
     // contact scroll event
-    if(windowTop > (pfScrollT + 300)){
+    if(windowTop > pfScrollT){
       $('#portfolio li:nth-child(5) a').animate({
         top : '0'
-      }, 1000, "easeOutQuad");
+      }, 800, "easeOutQuad");
       $('#portfolio li:nth-child(6) a').animate({
         top : '0'
-      }, 1400, "easeOutQuad");
+      }, 1000, "easeOutQuad");
       $('#portfolio li:nth-child(7) a').animate({
         top : '0'
-      }, 1800, "easeOutQuad");
+      }, 1200, "easeOutQuad");
       $('#portfolio li:nth-child(8) a').animate({
         top : '0'
-      }, 2200, "easeOutQuad");
+      }, 1400, "easeOutQuad");
     }// contact scroll event end
 
   })// scroll event end
